4a17f9e1e55622741420e261ea0821e7f51b2d64
Kevin Urrutia <urrutia.kevin@gmail.com>
2012-09-05 22:08:33 -0700

    added images and fixed position

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module Splowse
     config.serve_static_assets = true;
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
