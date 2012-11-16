33d3d38322d4a9ff07597b92029f1dd23440a07f
Kevin Urrutia <urrutia.kevin@gmail.com>
2012-11-15 19:18:22 -0800

    files

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,21 +17,15 @@ set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command} "
+      run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
-
-  # task :cold do
-  #   deploy.update
-  #   deploy.start
-  # end
 
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
@@ -41,6 +35,11 @@ namespace :deploy do
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
